4a48f82ce09899cb2db588cd663b66fc55dca2d2
AnastasiaFox1 <a.petrovicheva56@gmail.com>
2023-05-31 11:23:26 +0300

    site

--- script.js
+++
@@ -1,8 +0,0 @@
-var secondname = " ";
-
-document.getElementById('btn').addEventListener("click", Rename);
-
-function Rename(){
-	secondname = document.getElementById('_secondname').value;
-	document.getElementById('secondname').innerText = "Фамилия: " + secondname;
-}
